c724a76c27e156afd433d2c90cd91a103285184b
Ryne Vitek <rjvitek@hotmail.com>
2015-11-13 22:51:13 +0000

    Lesson12

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
 	def index
-		@places = Place.order(:id).page(params[:page]).per(5)
+		@places = Place.order(:id).page(params[:page]).per(4)
 	end
 end
