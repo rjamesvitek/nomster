d5274fb1f2efaaebe1d400f6351cf280b40e26e4
Ryne Vitek <rjvitek@hotmail.com>
2015-11-15 17:55:44 +0000

    Lesson28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :maximum => 140, :minimum => 3 }
+	validates :address, :presence => true, :length => { :maximum => 250, :minimum => 22 }
+	validates :description, :presence => true
 end
