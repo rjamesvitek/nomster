1123441f91e4833aa8e3405928dc2fd2804288b6
Ryne Vitek <rjvitek@hotmail.com>
2015-12-09 01:11:32 +0000

    TDD Lesson 9 Controller Tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -7,13 +7,14 @@ class CommentsControllerTest < ActionController::TestCase
 		sign_in user
 		place = FactoryGirl.create(:place)
 
-		#assert_difference 'Comment.count' do
+		assert_difference 'Comment.count' do
 			post :create, :place_id => place.id, :comment => {
 				:message => "This is a message.",
 				:rating => '1_star'
 				}
-		#end
-		#assert_equal 1, user.comments.count
+		end
+		assert_equal 1, user.comments.count
 		assert_redirected_to place_path(place)
+		#2
 	end
 end
